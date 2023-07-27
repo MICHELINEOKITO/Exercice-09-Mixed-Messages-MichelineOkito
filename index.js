fdf23a82529e7d0328f777b9c9fe891a086fa6e8
micheline okito <okitobibi2@gmail.com>
2023-07-27 05:16:52 -0700

    modification

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let  listeMessage = ["T'es jolie", "Dieu t'aime bien", "Ne sois jamais trop gentil"];
+let  listeMessage = ["T'es vraiment jolie", "Dieu t'aime bien", "Ne sois jamais trop gentil"];
 
 let messages=Math.floor(Math.random()*listeMessage.length)
 console.log(listeMessage[messages]);
