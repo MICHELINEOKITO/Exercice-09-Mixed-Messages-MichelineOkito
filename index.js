2374e509b97e73cfd36fbd4224ff6243437d9191
micheline okito <okitobibi2@gmail.com>
2023-07-27 05:08:41 -0700

    exercise aleatory message

---
+++ index.js
@@ -0,0 +1,6 @@
+let  listeMessage = ["T'es jolie", "Dieu t'aime bien", "Ne sois jamais trop gentil"];
+
+let messages=Math.floor(Math.random()*listeMessage.length)
+console.log(listeMessage[messages]);
+
+
